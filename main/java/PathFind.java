20eebd079e76e71c9e51ba2d8a4c3d01663cf35e
goodmorning <rkdrhksdn2@gmail.com>
2014-12-07 18:12:19 +0900

    create method that check to Place Number.

--- main/java/PathFind.java
+++ main/java/PathFind.java
@@ -15,6 +15,29 @@ public class PathFind {
                 .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
 
         String chosenNumber = scan.nextLine();
+        srcRegion = checkPlaceNumber(srcRegion, invalidInput, chosenNumber);
+        System.out
+                .println("where are you going? please choose your destination number.");
+
+        System.out
+                .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
+
+        String dstNumber = scan.nextLine();
+
+        invalidInput = true;
+        dstRegion = checkPlaceNumber(dstRegion, invalidInput, dstNumber);
+
+
+        int m = 30000; // 30000 경로가 없는 곳.
+        int[][] data = new int[][] { { 0, 3, 2, 6, m, 2 },
+                { 3, 0, 3, m, 8, m }, { 2, 3, 0, 2, 3, m },
+                { 6, m, 2, 0, 7, m }, { m, 8, 3, 7, 0, 1 },
+                { 2, m, m, m, 1, 0 }, };
+        Dijkstra k = new Dijkstra(data);
+        k.start(srcRegion, dstRegion);
+    }
+
+    public static int checkPlaceNumber(int srcRegion, boolean invalidInput, String chosenNumber) {
         while (invalidInput) {
             if (chosenNumber.equals("1") || chosenNumber.equals("seoul")
                     || chosenNumber.equals("Seoul")) {
@@ -47,56 +70,7 @@ public class PathFind {
                 System.out.println("input Text is invalid. you can try again");
             }
         }
-        System.out
-                .println("where are you going? please choose your destination number.");
-
-        System.out
-                .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
-
-        String dstNumber = scan.nextLine();
-
-        invalidInput = true;
-        while (invalidInput) {
-            if (dstNumber.equals("1") || dstNumber.equals("seoul")
-                    || dstNumber.equals("Seoul")) {
-                dstRegion = 1;
-                invalidInput = false;
-            } else if (dstNumber.equals("2")
-                    || dstNumber.equals("gangneung")
-                    || dstNumber.equals("Gangneung")) {
-                dstRegion = 2;
-                invalidInput = false;
-            } else if (dstNumber.equals("3")
-                    || dstNumber.equals("daejeon")
-                    || dstNumber.equals("Daejeon")) {
-                dstRegion = 3;
-                invalidInput = false;
-            } else if (dstNumber.equals("4")
-                    || dstNumber.equals("gwangju")
-                    || dstNumber.equals("Gwangju")) {
-                dstRegion = 4;
-                invalidInput = false;
-            } else if (dstNumber.equals("5") || dstNumber.equals("busan")
-                    || dstNumber.equals("Busan")) {
-                dstRegion = 5;
-                invalidInput = false;
-            } else if (dstNumber.equals("6") || dstNumber.equals("jeju")
-                    || dstNumber.equals("Jeju")) {
-                dstRegion = 6;
-                invalidInput = false;
-            } else {
-                System.out.println("input Text is invalid. you can try again");
-            }
-        }
-
-
-        int m = 30000; // 30000 경로가 없는 곳.
-        int[][] data = new int[][] { { 0, 3, 2, 6, m, 2 },
-                { 3, 0, 3, m, 8, m }, { 2, 3, 0, 2, 3, m },
-                { 6, m, 2, 0, 7, m }, { m, 8, 3, 7, 0, 1 },
-                { 2, m, m, m, 1, 0 }, };
-        Dijkstra k = new Dijkstra(data);
-        k.start(srcRegion, dstRegion);
+        return srcRegion;
     }
 }
 
--- test/java/PathFindTest.java
+++ test/java/PathFindTest.java
@@ -14,6 +14,30 @@ public class PathFindTest {
                 .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
 
         String chosenNumber = "1";
+        srcRegion = checkPlaceNumber(srcRegion, invalidInput, chosenNumber);
+        System.out
+                .println("where are you going? please choose your destination number.");
+
+        System.out
+                .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
+
+        String dstNumber = "6";
+
+        invalidInput = true;
+        dstRegion = checkPlaceNumber(dstRegion, invalidInput, dstNumber);
+
+
+        int m = 30000; // 30000 경로가 없는 곳.
+        int[][] data = new int[][] { { 0, 3, 2, 6, m, 2 },
+                { 3, 0, 3, m, 8, m }, { 2, 3, 0, 2, 3, m },
+                { 6, m, 2, 0, 7, m }, { m, 8, 3, 7, 0, 1 },
+                { 2, m, m, m, 1, 0 }, };
+        Dijkstra k = new Dijkstra(data);
+
+        return k.start(srcRegion, dstRegion);
+    }
+
+    public int checkPlaceNumber(int srcRegion, boolean invalidInput, String chosenNumber) {
         while (invalidInput) {
             if (chosenNumber.equals("1") || chosenNumber.equals("seoul")
                     || chosenNumber.equals("Seoul")) {
@@ -46,57 +70,7 @@ public class PathFindTest {
                 System.out.println("input Text is invalid. you can try again");
             }
         }
-        System.out
-                .println("where are you going? please choose your destination number.");
-
-        System.out
-                .println("1.Seoul 2.Gangneung 3.daejeon 4.gwangju 5.Busan 6.Jeju");
-
-        String dstNumber = "6";
-
-        invalidInput = true;
-        while (invalidInput) {
-            if (dstNumber.equals("1") || dstNumber.equals("seoul")
-                    || dstNumber.equals("Seoul")) {
-                dstRegion = 1;
-                invalidInput = false;
-            } else if (dstNumber.equals("2")
-                    || dstNumber.equals("gangneung")
-                    || dstNumber.equals("Gangneung")) {
-                dstRegion = 2;
-                invalidInput = false;
-            } else if (dstNumber.equals("3")
-                    || dstNumber.equals("daejeon")
-                    || dstNumber.equals("Daejeon")) {
-                dstRegion = 3;
-                invalidInput = false;
-            } else if (dstNumber.equals("4")
-                    || dstNumber.equals("gwangju")
-                    || dstNumber.equals("Gwangju")) {
-                dstRegion = 4;
-                invalidInput = false;
-            } else if (dstNumber.equals("5") || dstNumber.equals("busan")
-                    || dstNumber.equals("Busan")) {
-                dstRegion = 5;
-                invalidInput = false;
-            } else if (dstNumber.equals("6") || dstNumber.equals("jeju")
-                    || dstNumber.equals("Jeju")) {
-                dstRegion = 6;
-                invalidInput = false;
-            } else {
-                System.out.println("input Text is invalid. you can try again");
-            }
-        }
-
-
-        int m = 30000; // 30000 경로가 없는 곳.
-        int[][] data = new int[][] { { 0, 3, 2, 6, m, 2 },
-                { 3, 0, 3, m, 8, m }, { 2, 3, 0, 2, 3, m },
-                { 6, m, 2, 0, 7, m }, { m, 8, 3, 7, 0, 1 },
-                { 2, m, m, m, 1, 0 }, };
-        Dijkstra k = new Dijkstra(data);
-
-        return k.start(srcRegion, dstRegion);
+        return srcRegion;
     }
 }
 
