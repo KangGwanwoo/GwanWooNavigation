d8f8e483cfeec27819c6a17fa1b493de72adaa76
goodmorning <rkdrhksdn2@gmail.com>
2014-12-18 14:27:08 +0900

    실패한 테스트 .

--- test/java/PathFindTest.java
+++ test/java/PathFindTest.java
@@ -8,12 +8,24 @@ public class PathFindTest {
     public int start(String srcRegion, String dstRegion, String vehicle) {
         int time;
 
-        Dijkstra k = new Dijkstra();
+        DijkstraTest k = new DijkstraTest();
 
         int leastDistance =  k.start(Integer.parseInt(srcRegion), Integer.parseInt(dstRegion));
 
-
+        VehicleTest vehicleClass = selectVehicle(vehicle);
+        vehicleClass.setDistance(leastDistance);
+        time = vehicleClass.getTime();
         return leastDistance;
+    }
+
+    private VehicleTest selectVehicle(String vehicle) {
+        VehicleTest vehicleClass = null;
+        switch (Integer.parseInt(vehicle)){
+            case 1 : vehicleClass = new Airplane();
+                     break;
+        }
+
+        return vehicleClass;
     }
 
 }
