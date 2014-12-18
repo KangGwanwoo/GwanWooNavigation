e72509fa5f2107f7b9f69352686cc875250649c3
goodmorning <rkdrhksdn2@gmail.com>
2014-12-18 17:24:05 +0900

    delete Test code

--- test/java/VehicleTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by goodmorning on 2014-12-11.
- */
-public interface VehicleTest {
-    public int Speed = 0;
-    public int getSpeed();
-    public double getTime();
-    public void setDistance(int distance);
-}
